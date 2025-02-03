bc31c3f0eefbe0bd82662d91ea5c88e49431d3da
Tewos-21 <tewodrosshimels54@gmail.com>
2025-02-03 07:32:36 -0800

    debug the toggle

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/****
+ * navbar toggle in mobile
+ */
+
+const $navbar = document.querySelector("[data-navbar]");
+const $navToggler = document.querySelector("[data-nav-toggler]");
+
+$navToggler.addEventListener("click", () => $navbar.classList.toggle("active"));
