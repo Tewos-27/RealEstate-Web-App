6f936567ebfc54944fd219d3e48a055693fb0f98
Tewos-21 <tewodrosshimels54@gmail.com>
2025-02-06 02:51:49 -0800

    add a scroll functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,16 @@ const $navbar = document.querySelector("[data-navbar]");
 const $navToggler = document.querySelector("[data-nav-toggler]");
 
 $navToggler.addEventListener("click", () => $navbar.classList.toggle("active"));
+
+
+/***
+ * 
+ 
+Header scrolls state
+ */
+
+const $header =  document.querySelectorAll("[data-header]");
+window.addEventListener("scroll", e => {
+    $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+    
+})
