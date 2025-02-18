373a7412d36881bf7fe594956cecb281f3a8faa9
Tewos-21 <tewodrosshimels54@gmail.com>
2025-02-18 03:08:05 -0800

    add changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,18 @@
 'use strict';
-
 /****
- * navbar toggle in mobile
- */
-
+navbar toggle in mobile
+*/
 const $navbar = document.querySelector("[data-navbar]");
 const $navToggler = document.querySelector("[data-nav-toggler]");
 
 $navToggler.addEventListener("click", () => $navbar.classList.toggle("active"));
-
-
 /***
- * 
- 
 Header scrolls state
- */
-
+ ***/
 const $header =  document.querySelectorAll("[data-header]");
 window.addEventListener("scroll", e => {
     $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
-    
-})
+});
 
 /**add to favorite button toggle */
 
@@ -28,7 +20,6 @@ const $toggleBtns = document.querySelectorAll("[data-toggle-btn]");
 
 $toggleBtns.forEach($toggleBtn => {
     $toggleBtn.addEventListener("click", () => {
-        $toggleBtn.classList.toggle("active");
-        
-    })
-})
+        $toggleBtn.classList.toggle("active");       
+    });
+});
