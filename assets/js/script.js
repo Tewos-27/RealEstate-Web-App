3fadce7542372a23c751bb3b33a6af50c25493db
Tewos-21 <tewodrosshimels54@gmail.com>
2025-04-14 22:49:24 -0700

    update changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,7 @@ window.addEventListener("scroll", e => {
     $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
 });
 
-/**add to favorite button toggle */
+/*add to favorite button toggle */
 
 const $toggleBtns = document.querySelectorAll("[data-toggle-btn]");
 
